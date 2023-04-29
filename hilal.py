d8f4475896dda4bcea3fa36438200f81c4fed8c9
hilalkodland1224 <132136205+hilalkodland1224@users.noreply.github.com>
2023-04-29 13:44:32 +0300

    Create hilal.py

---
+++ hilal.py
@@ -0,0 +1,12 @@
+meme_dict = {
+            "CRINGE": "Garip ya da utandırıcı bir şey",
+            "LOL": "Komik bir şeye verilen cevap",
+            }
+
+
+word = input("Anlamadığınız bir kelime yazın (hepsini büyük harflerle yazın!): ")
+
+if word in meme_dict:
+    print("Var")
+else:
+    print("Yok")
